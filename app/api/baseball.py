ceaa9e495c5094bf431e588dcfc1f16a97f09cbf
GwangHyeok <ewq15651@gmail.com>
2023-02-26 13:36:14 +0900

    feat: add baseball all API

--- app/api/baseball.py
+++ app/api/baseball.py
@@ -3,7 +3,7 @@ import jwt
 import time
 import app.db as db
 from datetime import datetime
-from config import app,build_actual_response,build_preflight_response
+from config import app,build_actual_response,build_preflight_response,result_make
 from ..swagger import baseball_api, Resource
 import app.swagger as sg
 
@@ -20,3 +20,66 @@ class baseballCreate(Resource):
 
 
         return "test"
+
+# 직관 정보 전체 조회
+@baseball_api.route("/all/<int:userIdx>")
+class baseballSearchAll(Resource):
+    @baseball_api.doc('직관정보전체조회')
+    def get(self, userIdx):
+        res = {}
+        msg = 'success'
+        code = 200
+
+        if request.method == 'OPTIONS':
+            return build_preflight_response()
+        
+        baseball_data = db.Baseball.query.filter(db.Baseball.userIdx == userIdx).all()
+        user_data = db.User.query.filter_by(id = userIdx).first()
+
+        data_list = []
+
+        for row in baseball_data:
+            data_dict = {
+                "title": row.title,
+                "home": row.homeTeam,
+                "away": row.awayTeam,
+                "homeResult": row.homeResult,
+                "awayResult": row.awayResult,
+                "homeScore": row.homeScore,
+                "awayScore": row.awayScore,
+                "matchData": row.matchDate.strftime("%Y-%m-%d %H:%M:%S %A"),
+                "insertDate": row.insertDate.strftime("%Y-%m-%d %H:%M:%S %A"),
+                "id": row.id
+            }
+            data_list.append(data_dict)
+
+        # 승/패/승률 데이터를 계산합니다.
+        win_count = 0
+        lose_count = 0
+        taem = user_data.team
+
+        for row in baseball_data:
+            if taem == row.homeTeam:
+                if row.homeResult == "승":
+                    win_count += 1
+                else:
+                    lose_count += 1
+            
+            if taem == row.awayTeam:
+                if row.awayResult == "승":
+                    win_count += 1
+                else:
+                    lose_count += 1
+
+        total_count = win_count + lose_count
+        odds = round(win_count / total_count * 100, 1)
+
+        res = {
+            "data": data_list,
+            "stats": {
+                "win": win_count,
+                "lose": lose_count,
+                "odds": odds
+            }
+        }
+        return result_make(res, msg, code)
